f59164db46587bcfcce87758b36d04d981dfa5d4
No767 <73260931+No767@users.noreply.github.com>
2022-02-06 14:11:51 -0800

    Make Disquest-ORM and Disquest-Postgres Fully Async

--- disquest-orm.py
+++ disquest-orm.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 from sqlalchemy import (Column, Integer, MetaData, Table, create_engine, func,
                         select, BigInteger, Sequence)
+from sqlalchemy.ext.asyncio import create_async_engine
 
 
 class helper:
@@ -20,9 +21,9 @@ class disaccount:
         self.id = ctx.author.id
         self.gid = ctx.guild.id
 
-    def getxp(self):
+    async def getxp(self):
         meta = MetaData()
-        engine = create_engine("sqlite:///disquest/user.db")
+        engine = create_async_engine("sqlite+aiosqlite:///disquest/user.db")
         users = Table(
             "user",
             meta,
@@ -37,21 +38,23 @@ class disaccount:
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = select(users.c.xp).where(
-            users.c.id == self.id, users.c.gid == self.gid)
-        results = conn.execute(s).fetchone()
-        if results is None:
-            insert_new = users.insert().values(xp=0, id=self.id, gid=self.gid)
-            conn.execute(insert_new)
-        else:
-            for row in results:
-                return row
-        conn.close()
+        async with engine.connect() as conn:
+            s = select(users.c.xp).where(
+                users.c.id == self.id, users.c.gid == self.gid)
+            results = await conn.execute(s)
+            results_fetched = results.fetchone()
+            if results_fetched is None:
+                insert_new = users.insert().values(xp=0, id=self.id, gid=self.gid)
+                await conn.execute(insert_new)
+            else:
+                for row in results_fetched:
+                    return row
 
-    def setxp(self, xp):
+    async def setxp(self, xp):
         meta = MetaData()
-        engine = create_engine("sqlite:///disquest/user.db")
+        engine = create_async_engine(
+            "sqlite+aiosqlite:///disquest/user.db"
+        )
         users = Table(
             "user",
             meta,
@@ -66,20 +69,19 @@ class disaccount:
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        update_values = (
-            users.update()
-            .values(xp=xp) 
-            .filter(users.c.id == self.id) # check for the row that contains the user id and guild id
-            .filter(users.c.gid == self.gid) # to prevent overwritting the whole xp column
-        )
-        conn.execute(update_values)
-        conn.close()
+        async with engine.begin() as conn:
+            update_values = (
+                users.update()
+                .values(xp=xp)
+                .filter(users.c.id == self.id)
+                .filter(users.c.gid == self.gid)
+            )
+            await conn.execute(update_values)
 
-    def addxp(self, offset):
-        pxp = self.getxp()
+    async def addxp(self, offset):
+        pxp = await self.getxp()
         pxp += offset
-        self.setxp(pxp)
+        await self.setxp(pxp)
 
 
 class lvl:
@@ -94,11 +96,13 @@ class lvl:
 
 
 class DisQuest(commands.Cog):
-    def __init__(self, bot):
+    async def __init__(self, bot):
         self.bot = bot
         os.chdir(os.path.dirname(__file__))
         meta = MetaData()
-        engine = create_engine("sqlite:///disquest/user.db")
+        engine = create_async_engine(
+            "sqlite+aiosqlite:///disquest/user.db"
+        )
         Table(
             "user",
             meta,
@@ -113,7 +117,8 @@ class DisQuest(commands.Cog):
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        meta.create_all(engine)
+        async with engine.connect() as conn:
+            await conn.run_sync(meta.create_all)
 
     @commands.command(
         name="mylvl",
@@ -121,22 +126,24 @@ class DisQuest(commands.Cog):
     )
     async def mylvl(self, ctx):
         user = disaccount(ctx)
-        xp = user.getxp()
-        await ctx.channel.send(
-            embed=helper.fast_embed(
-                f"""User: {ctx.author.mention}
-        LVL. {lvl.cur(xp)}
-        XP {xp}/{lvl.next(xp)*100}"""
-            )
-        )
+        xp = await user.getxp()
+        embedVar = discord.Embed(color=discord.Color.from_rgb(255, 217, 254))
+        embedVar.add_field(
+            name="User", value=f"{ctx.author.mention}", inline=True)
+        embedVar.add_field(name="LVL", value=f"{lvl.cur(xp)}", inline=True)
+        embedVar.add_field(
+            name="XP", value=f"{xp}/{lvl.next(xp)*100}", inline=True)
+        await ctx.send(embed=embedVar)
 
     @commands.command(
         name="rank", help="Displays the most active members of your server!"
     )
     async def rank(self, ctx):
-        gid = discord.Guild.id
+        gid = ctx.guild.id
         meta = MetaData()
-        engine = create_engine("sqlite:///disquest/user.db")
+        engine = create_async_engine(
+            "sqlite+aiosqlite:///disquest/user.db"
+        )
         users = Table(
             "user",
             meta,
@@ -151,21 +158,22 @@ class DisQuest(commands.Cog):
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = (
-            select(Column("id", BigInteger), Column("xp", Integer))
-            .where(users.c.gid == gid)
-            .order_by(users.c.xp.desc())
-        )
-        results = conn.execute(s)
-        members = list(results.fetchall())
-        for i, mem in enumerate(members):
-            members[
-                i
-            ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
-        await ctx.send(
-            embed=helper.fast_embed(f"**Server Rankings**\n{''.join(members)}")
-        )
+        async with engine.connect() as conn:
+            s = (
+                select(Column("id", BigInteger), Column("xp", Integer))
+                .where(users.c.gid == gid)
+                .order_by(users.c.xp.desc())
+            )
+            results = await conn.execute(s)
+            members = list(results.fetchall())
+            for i, mem in enumerate(members):
+                members[
+                    i
+                ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
+            embedVar = discord.Embed(
+                color=discord.Color.from_rgb(254, 255, 217))
+            embedVar.description = f"**Server Rankings**\n{''.join(members)}"
+            await ctx.send(embed=embedVar)
 
     @commands.command(
         name="globalrank",
@@ -173,7 +181,9 @@ class DisQuest(commands.Cog):
     )
     async def grank(self, ctx):
         meta = MetaData()
-        engine = create_engine("sqlite:///disquest/user.db")
+        engine = create_async_engine(
+            "sqlite+aiosqlite:///disquest/user.db"
+        )
         users = Table(
             "user",
             meta,
@@ -188,22 +198,26 @@ class DisQuest(commands.Cog):
             Column("gid", BigInteger),
             Column("xp", Integer),
         )
-        conn = engine.connect()
-        s = (
-            select(Column("id", Integer), func.sum(users.c.xp).label("txp"))
-            .group_by(users.c.id)
-            .group_by(users.c.xp)
-            .order_by(users.c.xp.desc())
-        )
-        results = conn.execute(s).fetchall()
-        members = list(results)
-        for i, mem in enumerate(members):
-            members[
-                i
-            ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
-        await ctx.send(
-            embed=helper.fast_embed(f"**Global Rankings**\n{''.join(members)}")
-        )
+        async with engine.connect() as conn:
+            s = (
+                select(Column("id", Integer), func.sum(
+                    users.c.xp).label("txp"))
+                .group_by(users.c.id)
+                .group_by(users.c.xp)
+                .order_by(users.c.xp.desc())
+                .limit(10)
+            )
+            results = await conn.execute(s)
+            results_fetched = results.fetchall()
+            members = list(results_fetched)
+            for i, mem in enumerate(members):
+                members[
+                    i
+                ] = f"{i}. {(await self.bot.fetch_user(mem[0])).name} | XP. {mem[1]}\n"
+            embedVar = discord.Embed(
+                color=discord.Color.from_rgb(217, 255, 251))
+            embedVar.description = f"**Global Rankings**\n{''.join(members)}"
+            await ctx.send(embed=embedVar)
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
@@ -211,15 +225,7 @@ class DisQuest(commands.Cog):
             return
         user = disaccount(ctx)
         reward = random.randint(0, 20)
-        user.addxp(reward)
-        xp = user.getxp()
-        if lvl.near(xp) * 100 in range(xp - reward, xp):
-            await ctx.channel.send(
-                embed=helper.fast_embed(
-                    f"{ctx.author.mention} has reached LVL. {lvl.cur(xp)}"
-                ),
-                delete_after=10,
-            )
+        await user.addxp(reward)
 
 
 def setup(bot):
